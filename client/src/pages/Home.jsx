634deec9cbbd408ace87bc887e1bfef7bfc01ee7
saifmalik2004 <saifmalik7827@gmail.com>
2024-09-16 11:12:21 +0530

    add getpost

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,99 +3,80 @@ import React, { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router';
 import conf from '../conf/conf';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout, setUser,setOnlineUser, setsocketConnection } from '../redux/userSlice';
+import { logout, setUser, setOnlineUser, setsocketConnection } from '../redux/userSlice';
 import SideBar from '../components/SideBar';
-import logo from "../assets/Screenshot 2024-07-14 104856.png"
-import io from "socket.io-client"
+import logo from "../assets/Screenshot 2024-07-14 104856.png";
+import io from "socket.io-client";
+
 function Home() {
-  const user = useSelector(state=> state.user)
-  const dispatch = useDispatch()
-  const navigate= useNavigate()
-  const location= useLocation()
-
-
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
   
-    const URL = `${conf.backendUrl}/api/user-details`;
+  const URL = `${conf.backendUrl}/api/user-details`;
 
-    const fetchUserDetails = async () => {
-        try {
-            const response = await axios.get(URL, { withCredentials: true });
-            
-            dispatch(setUser(response.data.data))
-            
-            if (response.data.data.logout){
-              dispatch(logout())
-              navigate("/email")
-            }
+  const fetchUserDetails = async () => {
+    try {
+      const response = await axios.get(URL, { withCredentials: true });
+      dispatch(setUser(response.data.data));
 
+      if (response.data.data.logout) {
+        dispatch(logout());
+        navigate("/email");
+      }
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+      // Optionally, you can show a message to the user or redirect to login
+    }
+  };
 
-            
-        } catch (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                console.error('Server Error:', error.response.data);
-                console.error('Status Code:', error.response.status);
-                console.error('Response Data:', error.response.data);
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.error('Request Error:', error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.error('Error:', error.message);
-            }
-        }
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
+  // Socket connection
+  useEffect(() => {
+    const socketConnection = io(conf.backendUrl, {
+      auth: {
+        token: localStorage.getItem('token'),
+      },
+    });
+
+    socketConnection.on('onlineUser', (data) => {
+      dispatch(setOnlineUser(data));
+    });
+
+    dispatch(setsocketConnection(socketConnection));
+
+    return () => {
+      socketConnection.disconnect();
     };
+  }, [dispatch]);
 
-    useEffect(() => {
-        fetchUserDetails();
-    }, []);
+  const basePath = location.pathname === '/';
 
-    // sockect connection
-    useEffect(() => {
-       const socketConnection = io(conf.backendUrl,{
-        auth:{
-            token:localStorage.getItem('token')
-        }
-       })
+  return (
+    <div className="grid lg:grid-cols-[300px,1fr] h-screen max-h-screen">
+      {/* Sidebar */}
+      <section className={`bg-white ${!basePath && 'hidden'} lg:block`}>
+        <SideBar />
+      </section>
 
-       socketConnection.on('onlineUser',(data)=>{
-        
-        dispatch(setOnlineUser(data))
-       })
+      {/* Message section */}
+      <section className={`${basePath && 'hidden'}`}>
+        <Outlet />
+      </section>
 
-       dispatch(setsocketConnection(socketConnection))
-       return()=>{
-        socketConnection.disconnect()
-       }
-    }, []);
-
-
-    
-    const basePath=location.pathname==='/'
-
-    return (
-        <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-           <section className={`bg-white ${!basePath && "hidden"} lg:block`} >
-            <SideBar/>
-           </section >
-           {/* {messgae component} */}
-            <section className={`${basePath && 'hidden'}`}>
-                <Outlet />
-            </section>
-            <div className={` justify-center items-center flex-col gap-2  hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div >
-              <img
-                src={logo}
-                width={250}
-                alt='logo'
-               
-              />
-            </div>
-            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      {/* Welcome Screen */}
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? 'hidden' : 'lg:flex'}`}>
+        <div>
+          <img src={logo} width={250} alt="logo" />
         </div>
-            
-        </div>
-    );
+        <p className="text-lg mt-2 text-slate-500">Select user to send message</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- server/socket/index.js
+++ server/socket/index.js
@@ -12,6 +12,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
+    methods:["GET","POST"],
     credentials: true,
   },
 });
